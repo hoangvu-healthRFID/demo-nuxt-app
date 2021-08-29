d3d698ee53e8d882a8a7a63199b4feea3ebdac73
hoangvu-healthRFID <76925956+hoangvu-healthRFID@users.noreply.github.com>
2021-08-30 00:33:14 +1000

    test comment 5th from development

--- config/config.js
+++ config/config.js
@@ -50,7 +50,7 @@ module.exports = {
 	envConfig: {
 		hostname: process.env.ENV_HOST,
 		port: process.env.ENV_PORT,
-		version: '2.0.0 Build 24',
+		version: '2.0.0 Build 25',
 		name: 'Healthrfid Controlpoint UI',
 		mobile: '1.0.0'
 	},
