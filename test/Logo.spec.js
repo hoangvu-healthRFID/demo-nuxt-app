0d64334abe82ae6cd3b33bd95de192eeb39e3289
hoangvu-healthRFID <76925956+hoangvu-healthRFID@users.noreply.github.com>
2021-09-29 12:57:38 +1000

    update 2

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -13,6 +13,9 @@ describe('simple test', () => {
         expect(2+2).toBe(4);
   });
   test('simple add 1', () => {
-    expect(2+1).toBe(5);
+    expect(2+1).toBe(3);
+  });
+  test('simple add 2', () => {
+    expect(2+1).toBe(3);
   })
 })
